08d2c90ee94d8427f25daab9b93056366d991082
YeoWool <gkaudrl542@daum.net>
2018-07-14 21:16:41 +0900

    간단한 기능 구현 - 글 입력하고 버튼누르면 textView에 보이기

--- app/src/main/java/desipline/hello_world/MainActivity.java
+++ app/src/main/java/desipline/hello_world/MainActivity.java
@@ -1,7 +1,11 @@
 package desipline.hello_world;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView textView = (TextView) findViewById(R.id.textView);
+        final EditText editText = (EditText) findViewById(R.id.editText);
+        Button btn = (Button) findViewById(R.id.btn);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setText(editText.getText().toString());
+            }
+        });
+
+
     }
 }
