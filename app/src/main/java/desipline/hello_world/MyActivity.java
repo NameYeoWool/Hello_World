778f7311c004828fa97bf6ee936ae15046ef88fe
YeoWool <gkaudrl542@daum.net>
2018-07-14 22:14:24 +0900

    새로운 화면 생성

---
+++ app/src/main/java/desipline/hello_world/MyActivity.java
@@ -0,0 +1,27 @@
+package desipline.hello_world;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+
+        Button backBtn = (Button) findViewById(R.id.btn_back);
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MyActivity.this, "돌아가기 버튼이 눌렸어요", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        });
+
+
+    }
+}
