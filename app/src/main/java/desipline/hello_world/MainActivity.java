5fc2a455597ec59e8fa50477a072c40e970d1858
YeoWool <gkaudrl542@daum.net>
2018-07-14 21:38:12 +0900

    다른 앱 화면 호출 - Toast, google 인터넷 창, dial 창

--- app/src/main/java/desipline/hello_world/MainActivity.java
+++ app/src/main/java/desipline/hello_world/MainActivity.java
@@ -1,30 +1,52 @@
 package desipline.hello_world;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn1, btn2, btn3;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final TextView textView = (TextView) findViewById(R.id.textView);
-        final EditText editText = (EditText) findViewById(R.id.editText);
-        Button btn = (Button) findViewById(R.id.btn);
+        btn1 = (Button) findViewById(R.id.btn_toast);
+        btn2 = (Button) findViewById(R.id.btn_google);
+        btn3 = (Button) findViewById(R.id.btn_tel);
 
-        btn.setOnClickListener(new View.OnClickListener() {
+        btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                textView.setText(editText.getText().toString());
+                Toast.makeText(MainActivity.this,"시작 버튼이 눌렸어요",Toast.LENGTH_SHORT).show();
+
             }
-        });
+        });//end btn1
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://google.com"));
+                startActivity(myIntent);
+            }
+        });//end btn2
+
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:010-000-0000"));
+                startActivity(myIntent);
+            }
+        });//end btn3
 
 
-    }
+
+    }//end onCreate
 }
